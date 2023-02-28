1940f8c182d8564c0535e5027994417ffdffffed
hardiknegi98 <hardiknegi98@gmail.com>
2023-02-28 17:11:50 +0530

    controller error removal

--- OrderService/src/main/java/com/hardik/OrderService/controllers/OrderController.java
+++ OrderService/src/main/java/com/hardik/OrderService/controllers/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class OrderController {
     OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<Object> placeOrder(OrderRequest orderRequest) {
+    public ResponseEntity<Object> placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return new ResponseEntity<>("order placed successfully",HttpStatus.OK);
     }
