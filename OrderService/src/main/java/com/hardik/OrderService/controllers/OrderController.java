57388d7c86ad43ee7f78c34ae6372e619866d182
hardiknegi98 <hardiknegi98@gmail.com>
2023-03-06 23:22:41 +0530

    circuit breaker implemented in order service:place order endpoint

--- OrderService/src/main/java/com/hardik/OrderService/controllers/OrderController.java
+++ OrderService/src/main/java/com/hardik/OrderService/controllers/OrderController.java
@@ -1,11 +1,9 @@
 package com.hardik.OrderService.controllers;
 
-import com.hardik.OrderService.dto.OrderLineItemsDto;
 import com.hardik.OrderService.dto.OrderRequest;
 import com.hardik.OrderService.exceptions.OrderException;
-import com.hardik.OrderService.models.Order;
-import com.hardik.OrderService.models.OrderLineItems;
 import com.hardik.OrderService.services.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,13 +22,19 @@ public class OrderController {
     OrderService orderService;
 
     @PostMapping
+    @CircuitBreaker(name="inventory service",fallbackMethod="fallBackMethod")
     public ResponseEntity<Object> placeOrder(@RequestBody OrderRequest orderRequest) {
         try {
             orderService.placeOrder(orderRequest);
-            return new ResponseEntity<>("order placed successfully", HttpStatus.OK);
+            return new ResponseEntity<>("order placed successfully", HttpStatus.CREATED);
         }catch (OrderException orderException){
             logger.info(orderException.getMessage());
             return new ResponseEntity<>(orderException.getMessage(),HttpStatus.CONFLICT);
         }
+    }
+
+    public ResponseEntity<Object> fallBackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return new ResponseEntity<>("Order not placed! please try after some time. Error in communicating with inventory service"
+                ,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
